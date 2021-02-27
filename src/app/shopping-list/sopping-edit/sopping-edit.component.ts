dd0ef80a53d428f9356a625cc66997f6f2aba4ef
Pavel Yudin <pasha.judin@gmail.com>
2021-02-27 16:21:32 +0200

    components structure

---
+++ src/app/shopping-list/sopping-edit/sopping-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sopping-edit',
+  templateUrl: './sopping-edit.component.html',
+  styleUrls: ['./sopping-edit.component.css']
+})
+export class SoppingEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
