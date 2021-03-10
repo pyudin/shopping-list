8d8c72e0d7de6a3ddb912f4521bcd7a302e9d14d
Pavel Yudin <pasha.judin@gmail.com>
2021-03-10 23:27:20 +0200

    routes setup

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -27,6 +27,10 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(index: number): Recipe {
+    return this.recipes[index];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
